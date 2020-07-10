7e67563ece7121a3473868916932538bb159fb65
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2020-07-11 00:49:20 +0100

    create MultiplicationTask.js component and update MultiplicationPage.js

--- src/pages/MultiplicationPage.js
+++ src/pages/MultiplicationPage.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MultiplicationPage = () => {
-    return (
-        <div>Multiplication</div>
-    );
+import '../css/Tasks.css';
+
+import MultiplicationTask from '../components/MultiplicationTask';
+
+class MultiplicationPage extends Component {
+    render() {
+        let divs = [];
+        for (let i = 0; i < 4; i++) {
+            divs.push(<MultiplicationTask key={i} />)
+        }
+        return <div className="tasks">{divs}</div>
+    }
 }
 
 export default MultiplicationPage;
