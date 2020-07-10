3c8f8ec56c5b2bd007c7310fd61866b4cd6a7832
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2020-07-11 00:50:04 +0100

    update AdditionTask.js

--- src/components/AdditionTask.js
+++ src/components/AdditionTask.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-
 const AdditionTask = () => {
     const [number1, setNumber1] = useState("click me!");
     const [number2, setNumber2] = useState("click me!");
     return (
         <div className="task">
-            <div className="divNumber" onClick={() => setNumber1(Math.floor(Math.random() * 100))}>{number1}</div>
+            <div className="divNumber" onClick={() => setNumber1(Math.floor(Math.random() * 100))}>
+                {number1}
+            </div>
             <FontAwesomeIcon icon="plus" />
-            <div className="divNumber" onClick={() => setNumber2(Math.floor(Math.random() * 100))}>{number2}</div>
+            <div className="divNumber" onClick={() => setNumber2(Math.floor(Math.random() * 100))}>
+                {number2}
+            </div>
             <FontAwesomeIcon icon="equals" />
             <input type="text" maxLength="10" placeholder="your answer" onFocus="this.placeholder = ''" />
         </div >
     );
 }
-
 
 export default AdditionTask;
