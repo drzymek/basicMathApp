d48cd6f9c6b85adb5826fa621b8a7426eaa8c4a9
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2020-07-17 23:44:58 +0100

    update DivisionPage.js component

--- src/pages/DivisionPage.js
+++ src/pages/DivisionPage.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const DivisionPage = () => {
-    return (
-        <div>Division</div>
-    );
+import '../css/Tasks.css'
+
+import DivisionTask from '../components/DivisionTask';
+
+
+class DivisionPage extends Component {
+    render() {
+        let divs = [];
+        for (let i = 0; i < 4; i++) {
+            divs.push(<DivisionTask key={i} />)
+        }
+        return <div className="tasks">{divs}</div>
+    }
 }
 
 export default DivisionPage;
