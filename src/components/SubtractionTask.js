93252cbafb5f8ba03af5ec563ed7e4c2d525085f
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2020-07-17 23:44:29 +0100

    update SubtractionTask.js component

--- src/components/SubtractionTask.js
+++ src/components/SubtractionTask.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const SubtractionTask = () => {
-    const [number1, setNumber1] = useState(0);
-    const [number2, setNumber2] = useState(0)
+    const [number1, setNumber1] = useState("click me");
+    const [number2, setNumber2] = useState("click me!")
     return (
         <div className="task">
             <div className="divNumber" onClick={() => setNumber1(Math.floor(Math.random() * 100))}>{number1}</div>
