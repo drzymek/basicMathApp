ff28c3bf44c88575d8c6e131f7bba0138c5640d0
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2020-07-17 23:43:57 +0100

    create DivisionTask.js component

---
+++ src/components/DivisionTask.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const DivisionTask = () => {
+    const [number1, setNumber1] = useState("click me!");
+    const [number2, setNumber2] = useState("click me!");
+    return (
+        <div className="task">
+            <div className="divNumber" onClick={() => setNumber1(Math.floor(Math.random() * 10))}>
+                {number1}
+            </div>
+            <FontAwesomeIcon icon="divide" />
+            <div className="divNumber" onClick={() => setNumber2(Math.floor(Math.random() * 10))}>
+                {number2}
+            </div>
+            <FontAwesomeIcon icon="equals" />
+            <input type="text" maxLength="10" placeholder="your answer" onFocus="this.placeholder = ''" />
+        </div>
+    );
+}
+
+export default DivisionTask;
