4b6961be1bba3c5ae191c88f8994f65519ccf5c5
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2020-08-06 23:43:15 +0100

    update AdditionTask.js , add handleScore function

--- src/components/AdditionTask.js
+++ src/components/AdditionTask.js
@@ -1,20 +1,26 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const AdditionTask = () => {
+const AdditionTask = (props) => {
     const [number1, setNumber1] = useState("click me!");
     const [number2, setNumber2] = useState("click me!");
+    const [score, setScore] = useState("your answer");
+
+    const drawFirstNumber = () => setNumber1(Math.floor(Math.random() * 100));
+    const drawSecondNumber = () => setNumber2(Math.floor(Math.random() * 100));
+    const handleScore = (e) => setScore(e.target.value);
+
     return (
         <div className="task">
-            <div className="divNumber" onClick={() => setNumber1(Math.floor(Math.random() * 100))}>
+            <div className="divNumber" onClick={drawFirstNumber}>
                 {number1}
             </div>
             <FontAwesomeIcon icon="plus" />
-            <div className="divNumber" onClick={() => setNumber2(Math.floor(Math.random() * 100))}>
+            <div className="divNumber" onClick={drawSecondNumber}>
                 {number2}
             </div>
             <FontAwesomeIcon icon="equals" />
-            <input type="text" maxLength="10" placeholder="your answer" onFocus="this.placeholder = ''" />
+            <input type="text" maxLength="10" placeholder={score} onFocus="this.placeholder = ''" onChange={handleScore} />
         </div >
     );
 }
